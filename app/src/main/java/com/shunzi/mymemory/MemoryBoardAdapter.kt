62cd7ba5c0ac0a083898c9db3ec6e7c61d088f03
Kijimj7 <pinkokinawa84@gmail.com>
2021-01-07 21:52:09 -0800

    Initial commit

---
+++ app/src/main/java/com/shunzi/mymemory/MemoryBoardAdapter.kt
@@ -0,0 +1,66 @@
+package com.shunzi.mymemory
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.models.BoardSize
+import kotlin.math.min
+
+class MemoryBoardAdapter(private val context: Context,
+                         private val boardSize: BoardSize,
+                         //integer represents one of the drawable resources
+                         private val cardImages: List<Int>
+) :
+        RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+ companion object{
+     private const val MARGIN_SIZE = 10
+     private const val TAG = "MemoryBoardAdapter"
+
+ }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / boardSize.getWidth() - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / boardSize.getHeight() - (2 * MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+
+    }
+
+
+
+    // taking the data with the position and biding to the viewholder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    //How many view in our recyclerview
+    override fun getItemCount() = boardSize.numCards
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+
+
+        fun bind(position: Int) {
+            //reference inside the bind
+            imageButton.setImageResource(cardImages[position])
+            imageButton.setOnClickListener{
+                //position where cards' positions
+                Log.i(TAG, "Clicked on position $position")
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/utils/Constants.kt
@@ -0,0 +1,23 @@
+package com.utils
+
+import com.shunzi.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+        R.drawable.ic_1,
+        R.drawable.ic_2,
+        R.drawable.ic_3,
+        R.drawable.ic_4,
+        R.drawable.ic_5,
+        R.drawable.ic_6,
+        R.drawable.ic_7,
+        R.drawable.ic_8,
+        R.drawable.ic_8,
+        R.drawable.ic_8,
+        R.drawable.ic_8,
+        R.drawable.ic_8,
+
+
+
+
+
+)
